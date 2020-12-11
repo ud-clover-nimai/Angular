5f1caed5d35c6f01f8069615900c85e079ccda8a
Ashvini <ashvini.bangar@cloverinfotech.com>
2020-12-11 23:14:01 +0530

    bank dashboard top 5 changes etc.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
- domain:'http://136.232.244.190:8081'
+domain:'http://136.232.244.190:8081'
  //domain:'http://203.115.123.93:8080'
  //domain:'http://203.115.123.93:9090'
 };
